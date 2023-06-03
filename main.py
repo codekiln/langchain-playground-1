610acd8b5e15a3aca4c7956ba62ccaf1e2ad122a
Myer Nore <pnore@hbs.edu>
2023-06-03 14:43:13 -0400

    get sample_agents_with_chat_models

--- main.py
+++ main.py
@@ -155,6 +155,58 @@ def sample_chains_with_chat_models():
     # -> "J'aime programmer."
 
 
+def sample_agents_with_chat_models():
+    """
+    https://python.langchain.com/en/latest/getting_started/getting_started.html#agents-with-chat-models
+    Note - when I ran this the output was significantly different,
+    because the top search results have changed, which forced OpenAI to fall
+    back to Harry Stiles age back when it was trained (27). Compare this with their output,
+    which gives 29 (the correct answer).
+
+    agent.run("Who is Olivia Wilde's boyfriend? What is his current age raised to the 0.23 power?")
+    > Entering new AgentExecutor chain...
+    Thought: I need to use a search engine to find Olivia Wilde's boyfriend and a calculator to raise his age to the 0.23 power.
+    Action:
+    ```
+    {
+      "action": "Search",
+      "action_input": "Olivia Wilde boyfriend"
+    }
+    ```
+    Observation: Olivia Wilde started dating Harry Styles after ending her years-long engagement to Jason Sudeikis — see their relationship timeline.
+    Thought:Now I need to use a calculator to raise Harry Styles' age to the 0.23 power.
+    Action:
+    ```
+    {
+      "action": "Calculator",
+      "action_input": "pow(27, 0.23)"
+    }
+    ```
+    Observation: Answer: 2.1340945944237553
+    Thought:I have found the answer to the question.
+    Final Answer: Harry Styles' current age raised to the 0.23 power is 2.1340945944237553.
+    > Finished chain.
+    """
+    from langchain.agents import load_tools
+    from langchain.agents import initialize_agent
+    from langchain.agents import AgentType
+    from langchain.chat_models import ChatOpenAI
+    from langchain.llms import OpenAI
+
+    # First, let's load the language model we're going to use to control the agent.
+    chat = ChatOpenAI(temperature=0)
+
+    # Next, let's load some tools to use. Note that the `llm-math` tool uses an LLM, so we need to pass that in.
+    llm = OpenAI(temperature=0)
+    tools = load_tools(["serpapi", "llm-math"], llm=llm)
+
+    # Finally, let's initialize an agent with the tools, the language model, and the type of agent we want to use.
+    agent = initialize_agent(tools, chat, agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+
+    # Now let's test it out!
+    agent.run("Who is Olivia Wilde's boyfriend? What is his current age raised to the 0.23 power?")
+
+
 if __name__ == "__main__":
     llm = OpenAI(temperature=0.9)
     # get_socks_name(llm)
@@ -165,3 +217,4 @@ if __name__ == "__main__":
     # get_demo_conversation_chain()
     # get_message_completions_from_chat_model()
     # sample_chains_with_chat_models()
+    # sample_agents_with_chat_models()
